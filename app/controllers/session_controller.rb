2d40e0a4437acc84eb82fbffe071f5eef898e80d
Dmitry Gruzd <donotsendhere@gmail.com>
2012-05-21 23:45:04 +0400

    small changes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,10 @@
 class SessionController < ApplicationController
   def write
-    session[:time] = Time.now.to_i
+    session = {:time => Time.now.to_i}
     render :text => 'session is set'
   end
 
   def read
-    render :text => "SESSION:#{session[:time].inspect}"
+    render :text => "SESSION:#{session.inspect}"
   end
 end
